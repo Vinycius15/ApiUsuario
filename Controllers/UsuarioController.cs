b2668c8ed0cdb274a33a3216cccac9ee66ae50fa
vinycius <viny.antunes@gmail.com>
2023-02-17 21:36:24 -0300

    firt commit

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,48 @@
+﻿using ApiSistemaUsuario.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ApiSistemaUsuario.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        // GET: api/<UsuarioController>
+        [HttpGet]
+        public IEnumerable<UsuarioModel> Get()
+        {
+           List<UsuarioModel> usuarioModels = new List<UsuarioModel>();
+            usuarioModels.Add(new UsuarioModel() { Id=1, Name= "Vinycius", Email="Vinycius@vinycius" });
+            return usuarioModels;
+        }
+
+        // GET api/<UsuarioController>/5
+        [HttpGet("{id}")]
+        public UsuarioModel Get(int id)
+        {
+            UsuarioModel usuario = new UsuarioModel() { Id = 1, Name = "Vinycius", Email = "Vinycius@vinycius" };
+            return usuario;
+        }
+
+        // POST api/<UsuarioController>
+        [HttpPost]
+        public void Post([FromBody] UsuarioModel usuario)
+        {
+
+        }
+
+        // PUT api/<UsuarioController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] UsuarioModel usuario)
+        {
+        }
+
+        // DELETE api/<UsuarioController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
